76357037fbee065021f7e812cef595e2c63d49b6
Baar Alexander <baar.alexander@gmail.com>
2016-05-20 21:08:13 +0200

    - Add Mardown 'Create Link' functionality.

--- cm2016_ue4G1/src/at/fhj/swd/confmana/md/MarkdownTextEnhancer.java
+++ cm2016_ue4G1/src/at/fhj/swd/confmana/md/MarkdownTextEnhancer.java
@@ -2,12 +2,13 @@ package at.fhj.swd.confmana.md;
 
 import java.util.List;
 
+/*
 import at.fhj.swd.confmana.md.manipulate.HeadingHelper;
 import at.fhj.swd.confmana.md.manipulate.LinkHelper;
 import at.fhj.swd.confmana.md.manipulate.ListHelper;
 import at.fhj.swd.confmana.md.manipulate.MiscHelper;
 import at.fhj.swd.confmana.md.manipulate.QuotingHelper;
-import at.fhj.swd.confmana.md.manipulate.StyleHelper;
+import at.fhj.swd.confmana.md.manipulate.StyleHelper;*/
 
 /**
  * The purpose of this class is to ease the creation of markdown documents
@@ -20,12 +21,12 @@ public class MarkdownTextEnhancer {
 
 	// Members must be instantiated
 	
-	private static HeadingHelper headingHelper;
+	/*private static HeadingHelper headingHelper;
 	private static LinkHelper linkHelper;
 	private static ListHelper listHelper;
 	private static MiscHelper miscHelper;
 	private static QuotingHelper quotingHelper;
-	private static StyleHelper styleHelper;
+	private static StyleHelper styleHelper;*/
 	
 	// Link operation
 	
@@ -35,9 +36,8 @@ public class MarkdownTextEnhancer {
 	 * @param url ... what is that?
 	 * @return ... what is returned?
 	 */
-	public static String createMdLink(String linkDescription, String url){
-		// insert meaning full code
-		return null;
+	public static String createMdLink(String linkDescription, String url) {
+		return "[" + linkDescription + "](" + url + ")";
 	}
 	
 	// List operations
